d639283092fb961769d20b88b7a91c70dbedd70e
Nath <ananthapindika739@gmail.com>
2023-05-26 15:58:04 +0700

    First Commit

---
+++ pos-frontend/fe-client/src/components/layouts/constanst.js
@@ -0,0 +1,14 @@
+export const MENU_LIST = [
+    {
+        name: 'Home',
+        path: '/'
+    },
+    {
+        name: 'Transaction',
+        path: '/transaction'
+    },
+    // {
+    //     name: 'History',
+    //     path: '/history'
+    // }
+]
