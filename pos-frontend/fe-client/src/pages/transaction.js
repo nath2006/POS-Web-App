89b1d7be30d33d07fe555f81123a24b9da3a2ace
Nath <ananthapindika739@gmail.com>
2023-12-26 21:04:46 +0700

    Update transaction history feature

--- pos-frontend/fe-client/src/pages/transaction.js
+++ pos-frontend/fe-client/src/pages/transaction.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Layout from "@/components/layouts/Layout";
+import api from "@/api";
+import TranscationList from "@/components/elements/TransactionList/TranscastionList";
 
 export default function Transaction() {
+  const [ transcationList, setTranscationList] = useState([])
+
+  const fetchTranscations = async () => {
+    try {  
+      const response = await api.get('/transactions');
+      const data = response.data.payload.transactions;
+      setTranscationList(data);
+    }catch (err) {
+      throw Error(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchTranscations();
+  }, [])
+
   return (
     <Layout >
-        <p>Transaction</p>
+        <h1>Transaction History</h1>
+        <TranscationList transcationList={transcationList}/>
     </Layout>
   )
 }
